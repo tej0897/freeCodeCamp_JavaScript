e87a80718ed6b1f6f0bf2444d8e09faa232b994f
Tejas Kumar S <tej0897@gmail.com>
2024-04-19 13:59:36 +0530

    Update script.js

--- RPG - Dragon Repeller/script.js
+++ RPG - Dragon Repeller/script.js
@@ -19,7 +19,9 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 const locations = [
   {
     name: "town square",
-    "button text": []
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see a sign that says \"Store\"."
   }
 ];
 
