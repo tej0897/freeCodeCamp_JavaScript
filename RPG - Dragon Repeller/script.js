70c6622903348a7080dede2fb8e53bc8d65f1871
Tejas Kumar S <tej0897@gmail.com>
2024-04-20 22:28:37 +0530

    Update script.js

--- RPG - Dragon Repeller/script.js
+++ RPG - Dragon Repeller/script.js
@@ -17,22 +17,10 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const weapons = [
-  {
-    name: "stick",
-    power: "5"
-  },
-  {
-    name: "dagger",
-    power: "30"
-  },
-  {
-    name: "claw hammer",
-    power: "50"
-  },
-  {
-    name: "sword",
-    power: "100"
-  }
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 }
 ];
 const locations = [
   {
@@ -98,7 +86,12 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-
+  if (gold >= 30) {
+    gold -= 30;
+    currentWeapon++;
+    goldText.innerText = gold;
+    text.innerText = "You now have a new weapon.";
+  }
 }
 
 function fightSlime() {
