a2fcb055d8932b372458bfffd18a2b286b3dea78
Tejas Kumar S <tej0897@gmail.com>
2024-04-22 16:54:57 +0530

    Update script.js

--- RPG - Dragon Repeller/script.js
+++ RPG - Dragon Repeller/script.js
@@ -164,9 +164,23 @@ function attack() {
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
   health -= monsters[fighting].level;
   monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
-  
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    defeatMonster();
+  }
 }
 
 function dodge() {
+
+}
+
+function defeatMonster() {
+
+}
+
+function lose() {
 
 }
